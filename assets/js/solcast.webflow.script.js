e4801c4899fdb9fe552f80605896eae46e421431
Rework-Digital <liam@reworkdigital.com.au>
2024-06-03 15:36:30 +1000

    Merge CSS and Custom JS into main repo
    * Add files via upload
    
    * Create css
    
    * Delete assets/css
    
    * Add files via upload
    
    * updating css file
    
    * Updating JS and CSS Files
    
    * Updating Splide script
    
    * Updated Function
    
    * Sync
    
    * StatusPal
    
    * Cleaned up function
    
    * Udpate to function
    
    * File Updates
    
    * Testing Call of Function
    
    * Updating updateCurrentLink Function
    
    * Adding jQuery Library
    
    * Splide Function Update
    
    * Update
    
    * Format updates
    
    * Removing jQuery from test folder
    
    * ESLint Install and fix.
    
    * Updated
    
    * Create .eslintrc.js
    
    * ESLint Fixed JS file
    
    * Defined Splide function in global comment

---
+++ assets/js/solcast.webflow.script.js
@@ -0,0 +1,106 @@
+/* global Splide */
+
+/*!
+ * Splide Script
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  new Splide('.splide', {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 9,
+    autoWidth: false,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    autoScroll: {
+      speed: 0.8,
+    },
+    breakpoints: {
+      1100: {
+        perPage: 6,
+      },
+      760: {
+        perPage: 4,
+      },
+      580: {
+        perPage: 3,
+      },
+      400: {
+        perPage: 2,
+      },
+    },
+  }).mount(window.splide.Extensions);
+});
+
+/*!
+ * Solar Radiation Maps
+ */
+
+/*!
+ * Latest World Map Embed
+ */
+// This function formats the date and time in the required format
+function formatDateTimeUTC() {
+  const now = new Date(); // Gets the current date and time
+  const year = now.getUTCFullYear();
+  const month = String(now.getUTCMonth() + 1).padStart(2, '0'); // Adds 1 because months start at 0
+  const day = String(now.getUTCDate()).padStart(2, '0');
+  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+  // Format: YYYY-MM-DDTHH:MM:00
+  return `${year}-${month}-${day}T${hours}:${minutes}:00`;
+}
+
+// This function updates the video URLs
+function updateVideoUrls() {
+  const dateTime = formatDateTimeUTC(); // Get the current formatted UTC date and time
+  const baseUrl = 'https://media.solcast.com/latest/global/1280x720';
+  // Update the poster and src attributes
+  const videoElement = document.querySelector('.hero_video');
+  videoElement.setAttribute('poster', `${baseUrl}.jpg?time=${dateTime}`);
+  videoElement.setAttribute('src', `${baseUrl}.mp4?time=${dateTime}`);
+}
+
+document.addEventListener('DOMContentLoaded', updateVideoUrls);
+
+/*!
+ * StatusPal.io required script
+ */
+window.statuspalWidget = {
+  subdomain: 'solcast-com',
+  badge: {
+    enabled: true,
+    selector: '.sp-status', // Optional
+    position: 'bottom', // Optional [top | bottom | left | right] - defaults to top.
+  },
+  banner: {
+    enabled: true,
+    position: 'bottom-left', // Optional [bottom-left | bottom-right | top-left | top-right], def: bottom-left
+    translations: {
+      en: {
+        lates_updates: 'View latest updates',
+        ongoing: 'Ongoing for {{time_diff}}',
+      },
+    },
+  },
+  // serviceId: 1, // Optional - Display the status of only one service
+};
+
+/*!
+ * Function to check if any dropdown link matches the current page
+ */
+
+function updateCurrentLink() {
+  const navDropdowns = document.querySelectorAll('.nav_dropdown');
+
+  navDropdowns.forEach((dropdown) => {
+    const links = dropdown.querySelectorAll('.nav_dropdown-list .nav_menu_link');
+    links.forEach((link) => {
+      if (link.href === window.location.href) {
+        dropdown.querySelector('.nav_menu_link.is-heading').classList.add('w--current');
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', updateCurrentLink);
