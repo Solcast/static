e62715f870062216d3329f505de2722c0fe6a8d3
Liam Hammersley <liam@reworkdigital.com.au>
2024-09-25 15:44:30 +1000

    Addition of Testimonial Slider config and update to logo splide.

--- assets/js/solcast.webflow.script.js
+++ assets/js/solcast.webflow.script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         speed: 0.8,
       },
       breakpoints: {
-        1100: {
+        1400: {
           perPage: 6,
         },
         760: {
@@ -142,3 +142,58 @@ function updateCurrentLink() {
 }
 
 document.addEventListener('DOMContentLoaded', updateCurrentLink);
+
+/*------------------------------*/
+/* Testimonial Slider Settings  */
+/*------------------------------*/
+
+$(".slider-main_component").each(function (index) {
+  let loopMode = true;
+  if ($(this).attr("loop-mode") === "true") {
+    loopMode = true;
+  }
+  let sliderDuration = 300;
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
+  }
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoHeight: true,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 2,
+    spaceBetween: "4%",
+    rewind: false,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
+    breakpoints: {
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "4%"
+      },
+      // tablet
+      768: {
+        slidesPerView: 1,
+        spaceBetween: "4%"
+      },
+      // desktop
+      992: {
+        slidesPerView: 2,
+        spaceBetween: "3%"
+      }
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+  });
+});
