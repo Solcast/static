9d447acaa3f7be01760e8a3ab1b18f894771bea7
Liam Hammersley <liam@reworkdigital.com.au>
2024-11-26 10:40:45 +1100

    Updates to CSS and JS files for new components

---
+++ assets/js/solcast.webflow.footer.js
@@ -0,0 +1,23 @@
+// Footer Newsletter Subscription Popup watcher to resize iFrame.
+
+const targetElement = document.querySelector('.footer_subscribe-popup');
+
+if (targetElement) {
+    // Create a new MutationObserver
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                const displayStyle = window.getComputedStyle(targetElement).display;
+                if (displayStyle === 'flex') {
+                    iFrameResize({log:true});
+                }
+            }
+        });
+    });
+
+    // Observer configuration: watch for attribute changes
+    const config = { attributes: true };
+
+    // Start observing the target element
+    observer.observe(targetElement, config);
+}
