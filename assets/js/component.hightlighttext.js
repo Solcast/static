fd9c381b69b0ce572344afd0c019a973ba433483
Rework-Digital <liam@reworkdigital.com.au>
2024-11-27 09:53:12 +1100

    Updates to CSS file and new JS for new Components (#34)
    * Hero Counter additon and update to main script file.
    
    * Updated gap on Proof Splide
    
    * Updated Gap
    
    * Update to main script
    
    * Updated Splide JS
    
    * Update to Testimonial Slider to fix mobile view
    
    * Splitting component scripts out
    
    * Blog Template Page customisations and scripts.
    
    * Update to CSS file
    
    * Updated Class Selector for Section Banner
    
    * Refactored CSS code to remove !important
    
    * Updated comments on css file.
    
    * Update to CSS formatting
    
    * Updates to CSS and JS files for new components
    
    * update to css file
    
    * Fix issues in css file
    
    * Splitting Font Update CSS

--- assets/js/component.hightlighttext.js
+++ assets/js/component.hightlighttext.js
@@ -4,13 +4,15 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     // Select all elements with the class 'script-text-highlight'
-    var elements = document.querySelectorAll('.script-text-highlight');
+
+    var elements = document.querySelectorAll('.text-rich-text');
+
   
     // Loop through each element and apply the text replacement
     elements.forEach(function(element) {
         // Use a regular expression to find the ## content ##
         var elementText = element.innerHTML;
-        var newText = elementText.replace(/##(.*?)##/g, '<span class="highlight">$1</span>');
+        var newText = elementText.replace(/##(.*?)##/g, '<span class="text-highlight">$1</span>');
         
         // Update the element with the new content
         element.innerHTML = newText;
