0fd8acabb0799fb0e90194adac3231cbbba67f65
Rework-Digital <liam@reworkdigital.com.au>
2025-07-03 13:23:52 +1000

    Addition of script for embedded interactive tables. (#47)
    * UTM Loader for Forms
    
    * Udpdated UTM Loader Script for Cookies
    
    * Addition of Interactive Map Script

---
+++ assets/js/embed.interactivemap.js
@@ -0,0 +1,14 @@
+!(function () {
+"use strict";
+window.addEventListener("message", function (a) {
+    if (void 0 !== a.data["datawrapper-height"]) {
+    var e = document.querySelectorAll("iframe");
+    for (var t in a.data["datawrapper-height"])
+        for (var r, i = 0; (r = e[i]); i++)
+        if (r.contentWindow === a.source) {
+            var d = a.data["datawrapper-height"][t] + "px";
+            r.style.height = d;
+        }
+    }
+});
+})();
