b81143830b21577118ccdf092926133d089e61d4
Rework-Digital <liam@reworkdigital.com.au>
2026-01-06 19:37:16 +1100

    Create clarity.js

---
+++ assets/js/clarity.js
@@ -0,0 +1,7 @@
+/** Microsoft Claity Script **/
+
+(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "uwj81e7e55");
